5a56f45d08fa304f9dda891e4a702e52261ab906
Jemish Ghoghari <jeg7076@thi.de>
2026-08-07 19:31:20 +0200

    feat: Update RMW implementation and ROS domain ID in entrypoint script

--- docker/scripts/entrypoint_additions/ws_entrypoint.sh
+++ docker/scripts/entrypoint_additions/ws_entrypoint.sh
@@ -9,6 +9,8 @@ source ${ISAAC_ROS_WS}/install/setup.bash
 export RCUTILS_COLORIZED_OUTPUT=1
 # export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 # export CYCLONEDDS_URI=/cyclonedds.xml
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export ROS_DOMAIN_ID=1
 
 # add robot model env variable
 export ROBOT_MODEL_NAME=rosbot_xl
